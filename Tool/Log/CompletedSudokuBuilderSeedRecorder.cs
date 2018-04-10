90ccfd8b5969ad3f32202523255a95b8797ef29b
ruenrback <runerback@hotmail.com>
2018-04-10 23:46:21 +0800

    repairing Log folder

--- Tool/Log/CompletedSudokuBuilderSeedRecorder.cs
+++ Tool/Log/CompletedSudokuBuilderSeedRecorder.cs
@@ -1,1 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
 
+namespace SudokuSolver
+{
+	public sealed class CompletedSudokuBuilderSeedRecorder : Log
+	{
+		public CompletedSudokuBuilderSeedRecorder()
+			: base("Completed Sudoku Builder Seeds.log")
+		{
+		}
+
+		public int GetLastRecord()
+		{
+			string recordLine = null;
+			bool locked = false;
+			foreach (var line in new MiscUtil.IO.ReverseLineReader(
+				() => new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite),
+				Encoding))
+			{
+				if (!locked && !base.TestLogStreamLock())
+				{
+					locked = true;
+					continue;
+				}
+				recordLine = line;
+				break;
+			}
+			return int.Parse(recordLine);
+		}
+
+		protected override void Disposing()
+		{
+		}
+	}
+}
